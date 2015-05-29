83eb072de8de04bb6c9c877f1a918805f7cb20ed
Ian <ian@iancmacdonald.com>
2015-05-29 11:34:56 -0700

    Changed functionality of cleanWhitespace to accept new lines or returns and treat them as separate words.

--- js/script.js
+++ js/script.js
@@ -10,33 +10,44 @@ $( document ).ready(function() {
 
 //raw js
 var findReplaceWord = function(word, newWord, string) {
-  var cleanString = cleanSentence(string);
-  var splitStrig = string.split(" ");
+  var splitString = cleanWhitespace(string);
+  var cleanString = cleanPunctuation(splitString);
+
   for (var i = 0; i < cleanString.length; i++) {
     if (cleanString[i] === word) {
-      cleanString[i] = newWord;
+      splitString[i] = newWord;
     }
   };
-  return cleanString;
+
+  return splitString.join(" ");
 };
 
-
 var cleanPunctuation = function(input) {
-  var string = input;
-  if (Array.isArray(string)) {
-    string = string.join(" ");
-  } else if (typeof string !== "string") {
-    return "Please input a string or an array."
-  }
-
+  var string = inputToString(input);
   var punctuationRegex = new RegExp('[^\\w\\s]','g');
   string = string.replace(punctuationRegex, '').toLowerCase();
   var words = string.split(" ");
   return words;
 };
 
-var cleanWhitespace = function(string) {
-  var regex = new RegExp('[^\\n\\r\\s]+', 'g');
+var cleanWhitespace = function(input) {
+  var string = inputToString(input);
+  // var regex = new RegExp('[^\\n\\r\\s]+', 'g');
+  var regex = new RegExp('([^\\s]+)|[\\r\\n]', 'g');
+
+  // ([^\s]+)|[\r\n]
   var words = string.match(regex);
   return words;
+};
+
+var inputToString = function (input) {
+  var string = "";
+  if (Array.isArray(input)) {
+    string = input.join(" ");
+  } else if (typeof input !== "string") {
+    return "Please input a string or an array."
+  } else {
+    string = input;
+  }
+  return string;
 };
--- spec/specs.js
+++ spec/specs.js
@@ -6,23 +6,44 @@ describe('cleanPunctuation', function() {
 });
 
 describe('cleanWhitespace', function() {
-  it("removes extraneous spaces, and new line or return characters, and returns an array of words", function() {
+  it("removes extraneous spaces, and returns an array of words, maintaining new lines or returs  as separate words", function() {
     var testSentence = 'It\'s my\n    watch\nat eight o\'clock   '
-    expect(cleanWhitespace(testSentence)).to.eql(['It\'s', 'my', 'watch', 'at', 'eight', 'o\'clock']);
+    expect(cleanWhitespace(testSentence)).to.eql(['It\'s', 'my', '\n', 'watch', '\n', 'at', 'eight', 'o\'clock']);
   });
 });
 
 describe('cleanWhitespace & cleanPunctuation', function() {
-  it("When used in concert, these methods remove extraneous spaces, and new line or return characters, as well as punctuation and returns an array of downcased words", function() {
+  it("When used in concert (whitespace then punctuation), these methods remove extraneous spaces, as well as punctuation and returns an array of downcased words , maintaining new lines or returs  as separate words", function() {
     var testSentence = 'It\'s my\n    watch\nat eight o\'clock   '
-    expect(cleanPunctuation(cleanWhitespace(testSentence))).to.eql(['its', 'my', 'watch', 'at', 'eight', 'oclock']);
+    expect(cleanPunctuation(cleanWhitespace(testSentence))).to.eql(['its', 'my', '\n', 'watch', '\n', 'at', 'eight', 'oclock']);
+  });
+
+  it("When used in concert (punctuation then whitespace), these methods remove extraneous spaces, as well as punctuation and returns an array of downcased words , maintaining new lines or returs  as separate words", function() {
+    var testSentence = 'It\'s my\n    watch\nat eight o\'clock   '
+    expect(cleanWhitespace(cleanPunctuation(testSentence))).to.eql(['its', 'my', '\n', 'watch', '\n', 'at', 'eight', 'oclock']);
   });
 });
 
+describe('inputToString', function() {
+  it("returns the input if the input is a string", function() {
+    var testSentence = 'It\'s my watch at eight o\'clock'
+    expect(inputToString(testSentence)).to.eql('It\'s my watch at eight o\'clock');
+  });
+
+  it("returns the input as a string if the input is an array", function() {
+    var testArray = ['It\'s', 'my', 'watch', 'at', 'eight', 'o\'clock']
+    expect(inputToString(testArray)).to.eql('It\'s my watch at eight o\'clock');
+  });
+
+  it("returns an error message if the input is neither a string or an array", function() {
+    var testObject = { name: 'Ian', ocupation: 'BAMF' }
+    expect(inputToString(testObject)).to.eql("Please input a string or an array.");
+  });
+});
 
 describe('findReplaceWord', function () {
   it("returns the original string with the word searched for replaced by the new word provided.", function() {
     var testSentence = 'My watch says It\'s my watch\nat eight o\'clock'
-    expect(findReplaceWord('watch', 'sextant', testSentence)).to.equal('My watch says It\'s my watch\nat eight o\'clock');
+    expect(findReplaceWord('watch', 'sextant', testSentence)).to.equal('My sextant says It\'s my sextant at eight o\'clock');
   });
 });
